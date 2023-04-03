72082f9163ad75018e1559347513e2f3168957b4
XXL999227 <1628805655@qq.com>
2023-04-03 22:56:16 +0800

    初始化仓库

---
+++ src/main/java/cn/xxl/platform/PlatformApplication.java
@@ -0,0 +1,19 @@
+package cn.xxl.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author xiaoxiaolong
+ * @since 2023/04/03
+ */
+@SpringBootApplication
+public class PlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformApplication.class, args);
+    }
+
+}
